9cfec0b098a69c5b521625fc0004040d4a89e546
revt0 <49416643+revt0@users.noreply.github.com>
2020-05-23 12:53:14 -0400

    Update 5 AD
    wokring on first map, added visual effect for speed  boost

--- GameJamMayUSC/Assets/Scripts/PlayerController.cs
+++ GameJamMayUSC/Assets/Scripts/PlayerController.cs
@@ -7,8 +7,15 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed;
+    [SerializeField] private float speed;
+    private float defaultSpeed = 10f;
+    private bool sbBool;
 
+    private float speedBoost;
+    [SerializeField] private Vector3 spawnPos;
+
+    //visual variables
+    public MeshRenderer boostRend;
     private int winNum;
     private int count;
     //public Text countText;
@@ -16,6 +23,9 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        speed = defaultSpeed;
+        spawnPos = new Vector3(0.0f, 0.5f, 0.0f);
+        speedBoost = 0;
         count = 0;
         //setCountText();
        //winText.text = "";
@@ -28,25 +38,67 @@ public class PlayerController : MonoBehaviour
     //Fixed input for movement
     private void FixedUpdate()
     {
-        
+        //movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        //end movement
+
+
+        //speed boost
+        if (speedBoost >= 180)
+        {
+            speed = defaultSpeed;
+            speedBoost = 0;
+            sbBool = false;
+            boostRend.enabled = false;
+        }
+        else if (sbBool)
+        {
+            
+            speedBoost++;
+        }
+        //end speedboost
     }
     //Collision with pick up objects
     private void OnTriggerEnter(Collider other)
     {
+        //Pick up counter
         if (other.gameObject.CompareTag("Pick Up"))
         {
+
             other.gameObject.SetActive(false);
             count++;
+            boostRend.enabled = true;
+            speed += defaultSpeed;
+            sbBool = true;
             //setCountText();
+        }
+
+        //
+        if (other.gameObject.CompareTag("Kill Box"))
+        {
+            respawn();
         }
     }
 
+    private void respawn ()
+    {
+        resetAll();
+        transform.position = spawnPos;
+        
+    }
+
+    private void resetAll ()
+    {
+        rb.Sleep();
+        speed = defaultSpeed;
+        count = 0;
+        rb.WakeUp();
+    }
     private void setCountText()
     {
         /*countText.text = "Count: " + count.ToString();
