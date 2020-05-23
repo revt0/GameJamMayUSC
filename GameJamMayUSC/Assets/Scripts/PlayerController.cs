5df163f31df33b1c69625e30252edbc9a3b62e12
revt0 <49416643+revt0@users.noreply.github.com>
2020-05-23 13:47:04 -0400

    Update 5.2

--- GameJamMayUSC/Assets/Scripts/PlayerController.cs
+++ GameJamMayUSC/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 //written by Andrew Denman
 public class PlayerController : MonoBehaviour
@@ -9,50 +6,89 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public float speed;
 
-    private int winNum;
-    private int count;
-    //public Text countText;
-    //public Text winText;
+    private float defaultSpeed = 10f;
+    private bool sbBool;
+    private float speedBoost;
+    [SerializeField] private Vector3 spawnPos;
+
+    //visual variables
+    public MeshRenderer boostRend;
+
+
+    public void ApplyMovement(InputPacket packet)
+    {
+        Vector3 movement = new Vector3(packet.horizontal, 0.0f, packet.vertical);
+        rb.AddForce(movement * speed);
+    }
 
     private void Start()
     {
-        count = 0;
-        //setCountText();
-       //winText.text = "";
-        winNum = 12;
-    }   
-    private void Update()
-    {
-        
+        speed = defaultSpeed;
+        spawnPos = new Vector3(0.0f, 0.5f, 0.0f);
+        speedBoost = 0;
+ 
     }
     //Fixed input for movement
     private void FixedUpdate()
     {
-        
+        //movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+        //end movement
+
+
+        //speed boost
+        if (speedBoost >= 180)
+        {
+            speed = defaultSpeed;
+            speedBoost = 0;
+            sbBool = false;
+            boostRend.enabled = false;
+        }
+        else if (sbBool)
+        {
+
+            speedBoost++;
+        }
+        //end speedboost
     }
     //Collision with pick up objects
     private void OnTriggerEnter(Collider other)
     {
+        //Pick up counter
         if (other.gameObject.CompareTag("Pick Up"))
         {
+
             other.gameObject.SetActive(false);
-            count++;
+
+            boostRend.enabled = true;
+            speed += defaultSpeed;
+            sbBool = true;
             //setCountText();
+        }
+
+        //
+        if (other.gameObject.CompareTag("Kill Box"))
+        {
+            respawn();
         }
     }
 
-    private void setCountText()
+    private void respawn()
     {
-        /*countText.text = "Count: " + count.ToString();
-        if (count >= winNum)
-        {
-            winText.text = "You Win!";
-        }*/
+        resetAll();
+        transform.position = spawnPos;
+
+    }
+
+    private void resetAll()
+    {
+        rb.Sleep();
+        speed = defaultSpeed;
+        rb.WakeUp();
     }
 }
---
+++ GameJamMayUSC/Assets/Scripts/Settings.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Application.targetFrameRate = 300;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Fullscreen();
+    }
+
+    private void Fullscreen()
+    {
+        if (Screen.fullScreenMode == FullScreenMode.Windowed)
+        {
+            Resolution monitorRes = Screen.resolutions[Screen.resolutions.Length - 1];
+            Screen.SetResolution(monitorRes.width, monitorRes.height, FullScreenMode.FullScreenWindow);
+            Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        }
+        else
+        {
+            Screen.SetResolution(1280, 720, false);
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+}
